dd522b907bb2376d30a735275bf7108fe71f2cff
Lordsionn <etienne.laskar74@gmail.com>
2015-06-08 10:57:25 +0200

    Ajout de FIFO

---
+++ FIFO_strategy.c
@@ -0,0 +1,39 @@
+#include "strategy.h"
+#include "low_cache.h"
+#include "time.h"
+
+
+//! Creation et initialisation de la stratégie (invoqué par la création de cache).
+void *Strategy_Create(struct Cache *pcache){
+
+}
+
+//! Fermeture de la stratégie.
+void Strategy_Close(struct Cache *pcache){
+
+}
+
+//! Fonction "réflexe" lors de l'invalidation du cache.
+void Strategy_Invalidate(struct Cache *pcache){
+
+}
+
+//! Algorithme de remplacement de bloc.
+struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache){
+return null;
+}
+
+//! Fonction "réflexe" lors de la lecture.
+void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pb){
+
+}
+
+//! Fonction "réflexe" lors de l'écriture.
+void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pb){
+
+}
+
+//! Identification de la stratégie.
+char *Strategy_Name(){
+	return "FIFO Strategy";
+}
