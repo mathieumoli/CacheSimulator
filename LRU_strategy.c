fccf34b51ef611e9db1e2ae65c29f1fb815f169a
JuIngong <flolaris@gmail.com>
2015-06-08 14:10:04 +0200

    ajout d'un define LRU qui transforme la strategie en un pointeur sur LRU

--- LRU_strategy.c
+++ LRU_strategy.c
@@ -4,36 +4,32 @@
 *
 */
 
-
 #include "cache_list.h"
 #include "strategy.h"
 #include "low_cache.h"
 
-int pointeurVersListe;
-int pointeurVersCache;
+#define LRU(pcache) ((struct Cache_List *)((pcache)->pstrategy))
 
 //! Creation et initialisation de la stratégie (invoqué par la création de cache).
 void *Strategy_Create(struct Cache *pcache){
-	pointeurVersCache= pcache;
-	pointeurVersListe= Cache_List_Create();
-	return pointeurVersListe;
+	return Cache_List_Create();
 }
 
 //! Fermeture de la stratégie.
 void Strategy_Close(struct Cache *pcache){
-	Cache_Liste_Delete((struct Cache_List *)((pcache)->pstrategy));
+	Cache_Liste_Delete(LRU(pcache));
 }
 
 //! Fonction "réflexe" lors de l'invalidation du cache.
 void Strategy_Invalidate(struct Cache *pcache){
-	Cache_List_Clear((struct Cache_List *)((pcache)->pstrategy));
+	Cache_List_Clear(LRU(pcache));
 }
 
 //! Algorithme de remplacement de bloc.
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache){
-	struct Cache_List *list = (struct Cache_List *)((pcache)->pstrategy);
+	struct Cache_List *list = LRU(pcache);
     struct Cache_Block_Header *blocInvalide;
-
+²
      /* je cherche un bloc invalide */
     if ((blocinvalide = Get_Free_Block(pcache)) != NULL)
     {
@@ -51,12 +47,12 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache){
 
 //! Fonction "réflexe" lors de la lecture.
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pb){
-	Cache_Liste_Move_To_End((struct Cache_List *)((pcache)->pstrategy), pb);
+	Cache_Liste_Move_To_End(LRU(pcache), pb);
 }
 
 //! Fonction "réflexe" lors de l'écriture.
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pb){
-	Cache_Liste_Move_To_End((struct Cache_List *)((pcache)->pstrategy), pb);
+	Cache_Liste_Move_To_End(LRU(pcache), pb);
 }
 
 //! Identification de la stratégie.
