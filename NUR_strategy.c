b0e65dcd88a7a7b31f88c68baf6a2d52b98bf428
PierreMassanes <pierre.massanes@gmail.com>
2015-06-08 16:05:27 +0200

    Correction NUR

--- NUR_strategy.c
+++ NUR_strategy.c
@@ -4,9 +4,10 @@
 struct pstrategy{
 	int nderef;
 	int cpt;
-}
+};
 
 void initialize_flag_R(struct Cache *pcache);
+int compute_RM(struct Cache_Block_Header *block);
 
 //! Creation et initialisation de la stratégie (invoqué par la création de cache).
 void *Strategy_Create(struct Cache *pcache){
@@ -28,8 +29,6 @@ void Strategy_Close(struct Cache *pcache){
 
 //! Fonction "réflexe" lors de l'invalidation du cache.
 void Strategy_Invalidate(struct Cache *pcache){
-	struct pstrategy *strategy = (struct pstrategy*)(pcache)->pstrategy;
-
 	// Réinitialisation du compteur et du bit REFER de chaque bloc du cache
 	initialize_flag_R(pcache);
 }
@@ -47,10 +46,10 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache){
 	// Si aucun bloc libre n'a été trouvé, on récupère le bloc NUR 
 	// (recherche du bloc dont la pondération rm est minimale)
     	for (i = 0 ; i < pcache->nblocks ; i++){
-		block = &pcache->headers[ib];
+		block = &pcache->headers[i];
 	
 		// Calcul de rm, rm = 2 * R + M
-		int rm = 2 * (block->flags & REFER) + (block->flags & MODIF);
+		int rm = compute_RM(block);
 
 		// rm = 0, le bloc n'a pas été utilisé ni modifié depuis le dernier intervalle
 		// Sinon on compare la pondération du bloc à la pondération minimale trouvée.
@@ -61,7 +60,8 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache){
 	    		block_NUR = block;
 		}	
     	}
-    return block_NUR; 
+
+	return block_NUR; 
 }
 
 //! Fonction "réflexe" lors de la lecture.
@@ -84,6 +84,7 @@ void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pb){
 		initialize_flag_R(pcache);
 	// On met le flag REFER à 1 (car accès en écriture)
 	pb->flags |= REFER;
+
 }
 
 //! Identification de la stratégie.
@@ -105,4 +106,14 @@ void initialize_flag_R(struct Cache *pcache){
 		// On augmente le nombre de déréférencements effectués
 	    	pcache->instrument.n_deref++;
 	}
+}
+
+int compute_RM(struct Cache_Block_Header *block){
+	int rm = 0;
+	if((block->flags & REFER) > 0)
+		rm += 2;
+	if((block->flags & MODIF) > 0)
+		rm += 1;
+	
+	return rm;
 }
