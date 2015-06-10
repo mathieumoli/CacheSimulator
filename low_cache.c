f8626ba20080eb3c6f26c732fff7f6f6b16acc1d
JuIngong <flolaris@gmail.com>
2015-06-10 08:20:27 +0200

    small low cache corection

--- low_cache.c
+++ low_cache.c
@@ -7,7 +7,7 @@ struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache){
 	int i ;
 	for(i = 0; i<pcache->nblocks; i++){
 		if(pcache->headers[i].flags &= ~VALID)
-			return pcache->headers[i];
+			return &(pcache->headers[i]);
 	}
 
 	return NULL;
